cf9a3ceefde20b138261e0f7bde1248f423d7bac
ning <ning@qq.com>
2019-12-14 20:35:35 +0800

    index-icons

--- travel/src/views/Home.vue
+++ travel/src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <Home-header/>
     <Home-swiper></Home-swiper>
+    <Home-icons></Home-icons>
   </div>
 </template>
 
@@ -9,11 +10,13 @@
 // @ is an alias to /src
 import HomeHeader from '@/components/home/Header'
 import HomeSwiper from '@/components/home/Swiper'
+import HomeIcons from '@/components/home/Icons'
 export default {
   name: 'home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
