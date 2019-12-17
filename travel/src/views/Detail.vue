29558f2aacce9d81ef21dc7eb055016fd0f65750
ning <ning@qq.com>
2019-12-18 04:03:14 +0800

    detail-list awit

--- travel/src/views/Detail.vue
+++ travel/src/views/Detail.vue
@@ -2,22 +2,43 @@
   <div>
     <Detail-banner></Detail-banner>
     <Detail-header></Detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <Detail-list :list="list"/>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from '@/components/detail/Banner'
 import DetailHeader from '@/components/detail/Header'
+import DetailList from '@/components/detail/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
   },
   data () {
     return {
-      code: ''
+      code: '',
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆联票 - 某一连锁店销售'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
     }
   },
   methods: {
